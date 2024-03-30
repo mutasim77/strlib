63474352701fc023b17f26a5e5114c1298054451
 <96326525+mutasim77@users.noreply.github.com>
2024-03-30 16:48:30 +0500

    feat: add new example

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 
 	str "github.com/mutasim77/strlib"
+	st2 "github.com/mutasim77/strlib/v2"
 )
 
 func main() {
 	reversed := str.Reverse("Hello, 世界! How are you?")
 	fmt.Println(reversed) // Output: ?uoy era woH !界世 ,olleH
+
+	// Count symbols in a string
+	count := st2.SymbolCount("Hello, 世界!")
+	fmt.Println(count) // Output: 10
 }
