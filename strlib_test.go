b15478b73a93777aa3faf3b2093d05351e478e56
 <96326525+mutasim77@users.noreply.github.com>
2024-03-30 17:58:47 +0500

    feat: add Testable examples for existing functions

--- strlib_test.go
+++ strlib_test.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 
 	str "github.com/mutasim77/strlib"
+	str2 "github.com/mutasim77/strlib/v2"
 )
 
 func ExampleReverse() {
 	reversed := str.Reverse("Hello, 世界!")
 	fmt.Println(reversed)
 	// Output: !界世 ,olleH
+}
+
+func ExampleSymbolCount() {
+	count := str2.SymbolCount("Hello, 世界!")
+	fmt.Println(count)
+	// Output: 10
 }
